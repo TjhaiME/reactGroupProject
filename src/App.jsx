27c685bd20658d663e2fede67de232795fcb89a0
TjhaiME <TME13.07.1994@Gmail.com>
2023-12-15 13:42:40 +1100

    added sort and updated and used button component for all buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import ReactButton from './Button';
 
 
+//let sortUpdated = false
 
 let initialTasks = {
   //FOR TESTING!!!!
@@ -25,6 +26,16 @@ let initialTasks = {
     "dateAdded" : -1,
     "priority" : -1, // NO PRIORITY
     "status" : 1, //nothing done yet, various states including: in-progress, completed, review
+    "assignedTo" : "me",//default
+    "notes" : "no notes yet"
+  },
+  "2" : {
+    "name" : "other name of task 2",
+    "description" : "no_task_description",
+    "dateDue" : -1, //-1 means NO DUE DATE, everyother number represents milliseconds since 1960 or whatever it is
+    "dateAdded" : -1,
+    "priority" : -1, // NO PRIORITY
+    "status" : -1, //nothing done yet, various states including: in-progress, completed, review
     "assignedTo" : "me",//default
     "notes" : "no notes yet"
   },
@@ -55,6 +66,7 @@ function App() {
   let [tasks, setTasks] = useState(initialTasks) //let tasks = {}
   //let isSorted = true
   let [isSorted,setIsSorted] = useState(true)
+  //sortUpdated = true
 
 
 
@@ -62,7 +74,7 @@ function App() {
 
   ///////////////////////////////////////
   //    Sorting        //
-  function get_sorted_keys(whatProperty="status"){
+  function get_sorted_keys(sortBool, whatProperty="status"){
 
     function compareStatus(a, b) {
       if (a.status < b.status) {
@@ -73,19 +85,41 @@ function App() {
       // a must be equal to b
       return 0;
     }
-
+    function compareKeys(a, b) {
+      if (parseInt(a.key) < parseInt(b.key)) {
+        return -1;
+      } else if (parseInt(a.key) > parseInt(b.key)) {
+        return 1;
+      }
+      // a must be equal to b
+      return 0;
+    }
     //could have other ones for priority etc
     let newObjs = []
-    for(let objKey of Object.keys(tasks)){
+    //for(let objKey of Object.keys(tasks)){
+    for(let index = 0; index < Object.keys(tasks).length; index++){
+      let objKey = Object.keys(tasks)[index]
       newObjs.push({})
-      newObjs[objKey]["status"] = tasks[objKey].status
-      newObjs[objKey]["key"] = objKey
+
+      //ADDING 2 CONSOLE LOGS FIXED A BUG....
+      //PERHAPS IT IS AN ASYNC ISSUE
+      console.log("key = ",objKey,", this task = ")
+      console.log(tasks[objKey])
+
+      newObjs[index]["status"] = tasks[objKey].status
+      newObjs[index]["key"] = objKey
     }
-    const sortedTempObjs = newObjs.toSorted(compareStatus)
+    let sortedTempObjs = []
+    if (sortBool == true){
+      sortedTempObjs = newObjs.toSorted(compareStatus)
+    }else{
+      sortedTempObjs = newObjs.toSorted(compareKeys)
+    }
     let sortedKeys = []
     for(let tempObj of sortedTempObjs){
       sortedKeys.push(tempObj.key)
     }
+ 
 
     return sortedKeys
   }////////////////////////////////////////
@@ -102,39 +136,57 @@ function App() {
   // }
 
 
+///////////////////////////////////////////////////////////////////////////
+//         Button functions
+//////////////////////////////////////////////
 
-
-  function add_task(taskElement){
+  const add_task = (taskElement) => {
     let newTasks = structuredClone(tasks)
     newTasks[String(nextID)] = taskElement //add new task
     setTasks(newTasks) //save to state variable
     nextID += 1
+    //make taskElement with from then use this function
   }
-  function remove_task(taskKey){
+  const remove_task = (taskKey) => {
+    // let sortBool = isSorted
+    // setIsSorted(false)
+    //some reason it doesnt work when isSorted == true
+    
     let newTasks = structuredClone(tasks)
     delete newTasks[taskKey]
     setTasks(newTasks) //save to state variable
+    
+    //setIsSorted(sortBool)
     //nextID += 1
   }
-  function remove_all_tasks(){
+  const remove_all_tasks = () => {
     console.log("deleting all tasks = ")
     console.log(tasks)
-    for (let taskKey of Object.keys(tasks)){
-      remove_task(taskKey)
-    }
+    // for (let taskKey of Object.keys(tasks)){
+    //   remove_task(taskKey)
+    // }
+    setTasks({})
     console.log("after deleting tasks = ")
     console.log(tasks)
   }
 
-
+///////////////////////////////////////////////////////////////////////////
   
 
   function get_actualTasksJSX(){
     let tasksJSX = []
 
     let orderedKeys = Object.keys(tasks)//unorderedKeys
+    if (orderedKeys.length == 0){
+      return <></> //null jsx
+    }
+
+    // if (sortUpdated == true){
+    //   sortUpdated = false
+    //   //testing if its an ansync issue
+    // }
     if (isSorted == true){
-      orderedKeys = get_sorted_keys()
+      orderedKeys = get_sorted_keys(true)
     }
     for(let taskKey of orderedKeys){ //tasks is an object
       const newTaskJSX = get_single_task_JSX(taskKey)
@@ -170,6 +222,11 @@ function App() {
         <div className='task_notes'>
           <h5>Notes:</h5>
           <p>{tasks[taskKey].notes}</p>
+        </div>
+
+        <div className='task_options'>
+          <h5>Options:</h5>
+          <ReactButton buttonFunc={remove_task} argument={taskKey} name={"Remove This"}/>
         </div>
 
       </div>
@@ -237,14 +294,16 @@ function App() {
   let toDoAppJSX = get_website_structure()
 
 
-  const button_clearAll = () => {
-    remove_all_tasks()
+  const button_toggleSort = () => {
+    setIsSorted(prev => !prev)
   }
 
   //RETURN APP.JSX
   return (
     <div className="App">
-      <ReactButton buttonFunc={button_clearAll}/>
+      {/* <ReactButton buttonFunc={button_clearAll}/> */}
+      <ReactButton buttonFunc={remove_all_tasks} argument={undefined} name={"Remove All"}/>
+      <ReactButton buttonFunc={button_toggleSort} argument={undefined} name={"Toggle Sort"}/>
       {toDoAppJSX}
       {/* <Custom_Task_Component/> */}
     </div>
--- src/Button.jsx
+++ src/Button.jsx
@@ -2,13 +2,33 @@
 //https://www.freecodecamp.org/news/how-to-use-props-in-react/
 
 import * as React from 'react'
+import { useEffect } from 'react'
 
 function ReactButton(props){
     const buttonFunc = props.buttonFunc
+    const argument = props.argument
+    const name = props.name
+
+    // useEffect(() => {
+    //     if (argument != undefined){
+
+    //         return (
+    //             <div>
+    //                 <button onClick={buttonFunc(argument)}>
+    //                     {name}
+    //                 </button>
+    //             </div>
+    //         )
+    
+    //     }
+    //   }, []
+    // );
+
+//else: buttonFunc(argument)
     return (
         <div>
-            <button onClick={buttonFunc}>
-                Click Me
+            <button onClick={() => {return buttonFunc(argument)}}>
+                {name}
             </button>
         </div>
     )
