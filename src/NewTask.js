31fe02831fc3fd21bd3ea7e4a79bb50c9cfa9dc7
Kash <141306723+kxxg1@users.noreply.github.com>
2023-12-18 12:36:47 +1100

    updated Todo App

---
+++ src/NewTask.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+import { format, parse } from 'date-fns';
+import './styles/NewTask.css';
+
+function NewTask({ addNewTask = () => {} }) {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [newTask, setNewTask] = useState({
+    title: '',
+    description: '',
+    taskDate: '',
+    taskTime: '',
+    priority: '',
+    assignee: '',
+    isReoccurring: false,
+    reoccurringDuration: '',
+    frequency: 'Weekly'
+  });
+
+  const formatTaskDate = (dateString) => {
+    if (!dateString) return '';
+    const date = parse(dateString, 'yyyy-MM-dd', new Date());
+    return format(date, 'MMM do yy');
+  };
+
+  const formatTaskTime = (timeString) => {
+    if (!timeString) return '';
+    const [hours, minutes] = timeString.split(':');
+    const time = new Date(0, 0, 0, hours, minutes);
+    return format(time, 'h:mm a');
+  };
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = (task) => {
+    let formErrors = {};
+    if (!task.title.trim()) formErrors.title = 'Title is required';
+    // Additional validations can be added here
+    return formErrors;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formErrors = validateForm(newTask);
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length === 0) {
+      addNewTask(newTask);
+      console.log(newTask);
+      setNewTask({
+        title: '', description: '', taskDate: '', taskTime: '', priority: '', assignee: '', isReoccurring: false, reoccurringDuration: '', frequency: 'Weekly'
+      });
+      setShowOverlay(false);
+    }
+  };
+
+  const openOverlay = () => setShowOverlay(true);
+  const closeOverlay = () => setShowOverlay(false);
+
+  return (
+    <div>
+      <button onClick={openOverlay} className="create-task-button">Create Task</button>
+
+      {showOverlay && (
+        <div className="overlay">
+          <form onSubmit={handleSubmit}>
+            <InputField label="Title" name="title" value={newTask.title} handleChange={handleChange} error={errors.title} />
+            <label>
+              Task Date: {formatTaskDate(newTask.taskDate)}
+              <input
+                type="date"
+                name="taskDate"
+                value={newTask.taskDate}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Task Time: {formatTaskTime(newTask.taskTime)}
+              <input
+                type="time"
+                name="taskTime"
+                value={newTask.taskTime}
+                onChange={handleChange}
+              />
+            </label>
+            <InputField label="Priority" type="select" name="priority" value={newTask.priority} handleChange={handleChange} options={["High", "Medium", "Low"]} />
+            <InputField label="Assignee" name="assignee" value={newTask.assignee} handleChange={handleChange} />
+            <label>
+              Description:
+              <textarea name="description" value={newTask.description} onChange={handleChange} maxLength="500"></textarea>
+            </label>
+            <label>
+              <input type="checkbox" name="isReoccurring" checked={newTask.isReoccurring} onChange={(e) => setNewTask({ ...newTask, isReoccurring: e.target.checked })} />
+              Reoccurring Task
+            </label>
+            {newTask.isReoccurring && (
+              <>
+                <label>
+                  Duration (Months):
+                  <input type="number" name="reoccurringDuration" value={newTask.reoccurringDuration} onChange={handleChange} min="1" max="6" />
+                </label>
+                <InputField
+                  label="Frequency"
+                  type="select"
+                  name="frequency"
+                  value={newTask.frequency}
+                  handleChange={handleChange}
+                  options={['Daily', 'Weekly', 'Fortnightly', 'Monthly']}
+                />
+              </>
+            )}
+            <button type="submit" className="submit-button">Add Task</button>
+            <button type="button" onClick={closeOverlay} className="cancel-button">Cancel</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function InputField({ label, type = "text", name, value, handleChange, error, options }) {
+  return (
+    <label>
+      {label}:
+      {type === "select" ? (
+        <select name={name} value={value} onChange={handleChange}>
+          <option value="">Select {label}</option>
+          {options && options.map(option => (
+            <option key={option} value={option}>{option}</option>
+          ))}
+        </select>
+      ) : (
+        <input type={type} name={name} value={value} onChange={handleChange} />
+      )}
+      {error && <span className="error">{error}</span>}
+    </label>
+  );
+}
+
+export default NewTask;
