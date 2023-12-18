cb465f221e6f4513a52856e0b0f72a6d7bf0c8c2
TjhaiME <TME13.07.1994@Gmail.com>
2023-12-18 14:18:54 +1100

    Local Commit

--- src/Button.jsx
+++ src/Button.jsx
@@ -5,7 +5,8 @@ import * as React from 'react'
 
 function ReactButton(props){
     const buttonFunc = props.buttonFunc
-    const argument = props.argument
+    const argument = props.argument //for some functions without input it is left as undefined
+    //To my suprise buttonFunc(undefined) works for a function that is meant to be called with no arguments
     const name = props.name
 
     return (
